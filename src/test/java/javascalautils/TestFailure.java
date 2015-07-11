d4551746aa00354609a02e4eb1dc574de0119fb1
Peter Nerg <peter.nerg@gmail.com>
2015-07-11 07:58:09 +0200

    Added tests for Success/Failure

---
+++ src/test/java/javascalautils/TestFailure.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package javascalautils;
+
+import org.junit.Test;
+
+/**
+ * Test the class {@link Failure}
+ * 
+ * @author Peter Nerg
+ */
+public class TestFailure extends BaseAssert {
+
+    private final Exception exception = new Exception("Error, terror!");
+    private final Try<String> t = new Failure<String>(exception);
+
+    @Test(expected = NullPointerException.class)
+    public void create_withNullValue() {
+        new Failure<Object>(null);
+    }
+
+    @Test
+    public void isSuccess() {
+        assertFalse(t.isSuccess());
+    }
+
+    @Test
+    public void isFailure() {
+        assertTrue(t.isFailure());
+    }
+
+    @Test(expected = Exception.class)
+    public void get() throws Throwable {
+        t.get();
+    }
+
+    @Test
+    public void getOrElse() {
+        String defaultText = "Default since we're testing a failure";
+        assertEquals(defaultText, t.getOrElse(() -> defaultText));
+    }
+
+    @Test
+    public void orElse() {
+        Success<String> defaultVal = new Success<String>("Default since we're testing a failure");
+        assertEquals(defaultVal, t.orElse(defaultVal));
+    }
+
+    @Test
+    public void failed() throws Throwable {
+        Try<Throwable> result = t.failed();
+        assertTrue(result.isSuccess());
+        assertEquals(exception, result.get());
+    }
+
+    @Test
+    public void toString_withValue() {
+        assertNotNull(t.toString());
+    }
+
+    @Test
+    public void asOption() {
+        assertFalse(t.asOption().isDefined());
+    }
+
+}
---
+++ src/test/java/javascalautils/TestSuccess.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package javascalautils;
+
+import org.junit.Test;
+
+/**
+ * Test the class {@link Success}
+ * 
+ * @author Peter Nerg
+ */
+public class TestSuccess extends BaseAssert {
+
+    private final String message = "Peter Rulez";
+    private final Try<String> t = new Success<String>(message);
+
+    @Test
+    public void isSuccess() {
+        assertTrue(t.isSuccess());
+    }
+
+    @Test
+    public void isFailure() {
+        assertFalse(t.isFailure());
+    }
+
+    @Test
+    public void get() throws Throwable {
+        assertEquals(message, t.get());
+    }
+
+    @Test
+    public void getOrElse() {
+        assertEquals(message, t.getOrElse(() -> "Ignore as we're successful"));
+    }
+
+    @Test
+    public void orElse() {
+        assertEquals(t, t.orElse(new Success<String>("Ignore as we're successful")));
+    }
+
+    @Test
+    public void failed() {
+        Try<Throwable> failed = t.failed();
+        assertTrue(failed.isFailure());
+    }
+
+    @Test
+    public void toString_withValue() {
+        assertNotNull(t.toString());
+    }
+
+    @Test
+    public void toString_withNullValue() {
+        assertNotNull(new Success<Object>(null).toString());
+    }
+
+    @Test
+    public void asOption() {
+        assertTrue(t.asOption().isDefined());
+    }
+}
