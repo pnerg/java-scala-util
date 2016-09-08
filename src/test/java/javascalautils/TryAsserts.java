cb91ca3752a18712061b6c27658d5f49b51558e0
Peter Nerg <peter.nerg@gmail.com>
2016-09-08 08:21:08 +0200

    added new test interface TryAsserts

---
+++ src/test/java/javascalautils/TryAsserts.java
@@ -0,0 +1,37 @@
+/* 
+ * Created : 9/8/16
+ * 
+ * Copyright (c) 2016 Ericsson AB, Sweden. 
+ * All rights reserved. 
+ * The Copyright to the computer program(s) herein is the property of Ericsson AB, Sweden. 
+ * The program(s) may be used and/or copied with the written permission from Ericsson AB 
+ * or in accordance with the terms and conditions stipulated in the agreement/contract 
+ * under which the program(s) have been supplied. 
+ */
+package javascalautils;
+
+import org.junit.Assert;
+
+/**
+ * Utility asserts related to the {@link javascalautils.Try} class.
+ * @author epknerg.
+ * @since 1.10
+ */
+public interface TryAsserts {
+
+    /**
+     * Asserts that the provided Try is a Success.
+     * @param t The Try to assert
+     */
+    default void assertIsSuccess(Try<?> t) {
+        Assert.assertTrue("The Try ["+t+"] is not a Success", t.isSuccess());
+    }
+
+    /**
+     * Asserts that the provided Try is a Failure.
+     * @param t The Try to assert
+     */
+    default void assertIsFailure(Try<?> t) {
+        Assert.assertTrue("The Try ["+t+"] is not a Failure", t.isFailure());
+    }
+}
